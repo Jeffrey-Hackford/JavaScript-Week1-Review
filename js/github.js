09c81a1d748d7bdea00075e6f9b071be9cc64403
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-05-27 10:00:07 -0700

    fix bug with user followers display

--- js/github.js
+++ js/github.js
@@ -7,9 +7,10 @@ exports.Github = function(){
 
 exports.Github.prototype.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
-    $('#showName').text(response.name);
-    $('#followers').text(response.followers + "followers.");
+    console.log(response.followers);
+    $('#showName').text("This users name is: " + response.name);
+    $('#showNameFollowers').text(response.name);
+    $('#followers').text(response.followers + " followers.");
 
   }).fail(function(error){
     console.log(error.responseJSON.message);
